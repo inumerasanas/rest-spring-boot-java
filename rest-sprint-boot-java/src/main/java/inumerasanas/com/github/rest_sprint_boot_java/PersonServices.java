46d66e357a9e25a206a933a1824f489d833c0dac
inumerasnas <inumerasanas@gmail.com>
2025-04-02 19:13:46 -0300

    Adicionadno mais verbos HTTP

--- rest-sprint-boot-java/src/main/java/inumerasanas/com/github/rest_sprint_boot_java/PersonServices.java
+++ rest-sprint-boot-java/src/main/java/inumerasanas/com/github/rest_sprint_boot_java/PersonServices.java
@@ -3,6 +3,8 @@ package inumerasanas.com.github.rest_sprint_boot_java;
 import inumerasanas.com.github.rest_sprint_boot_java.model.Person;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
 import java.util.logging.Logger;
 
@@ -11,6 +13,18 @@ public class PersonServices {
 
     private final AtomicLong counter = new AtomicLong();
     private final Logger logger = Logger.getLogger(PersonServices.class.getName());
+
+    public List <Person> findAll() {
+        logger.info("Finding all People!");
+        List<Person> people = new ArrayList<Person>();
+        for (int i = 0; i < 8; i++) {
+            Person person = mockPerson(i);
+            people.add(person);
+        }
+        return people;
+    }
+
+
 
     public Person findById(String id) {
         logger.info("Finding one Person!");
@@ -23,6 +37,30 @@ public class PersonServices {
         person.setGender("Feminino");
         return person;
 
+    }
+    public Person create(Person person) {
+        logger.info("Creating one Person!");
+        return person;
+    }
+
+
+    public Person update(Person person) {
+        logger.info("Updating one Person!");
+        return person;
+    }
+
+    public void delete(String id) {
+        logger.info("Deleting one Person!");
+    }
+
+    private Person mockPerson(int i) {
+        Person person = new Person();
+        person.setId(counter.incrementAndGet());
+        person.setFirstName("Name " + i);
+        person.setLastName("LastName " + i);
+        person.setAddress("Algum lugar por ai - Bahia - Brasil");
+        person.setGender("Feminino");
+        return person;
     }
 
 }
--- rest-sprint-boot-java/src/main/java/inumerasanas/com/github/rest_sprint_boot_java/controllers/PersonController.java
+++ rest-sprint-boot-java/src/main/java/inumerasanas/com/github/rest_sprint_boot_java/controllers/PersonController.java
@@ -4,13 +4,12 @@ package inumerasanas.com.github.rest_sprint_boot_java.controllers;
 import inumerasanas.com.github.rest_sprint_boot_java.PersonServices;
 import inumerasanas.com.github.rest_sprint_boot_java.model.Person;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
-import static org.springframework.http.MediaType.APPLICATION_NDJSON_VALUE;
+
 
 @RestController
 @RequestMapping("/person")
@@ -19,6 +18,17 @@ public class PersonController {
     @Autowired
     private PersonServices service;
 
+    //people
+    @RequestMapping(
+            method = RequestMethod.GET,
+            produces = APPLICATION_JSON_VALUE
+    )
+    public List<Person> findAll(){
+        return service.findAll();
+    }
+
+
+    //id
     @RequestMapping(value = "/{id}",
         method = RequestMethod.GET,
         produces = APPLICATION_JSON_VALUE
@@ -26,4 +36,30 @@ public class PersonController {
     public Person findById(@PathVariable("id") String id){
         return service.findById(id);
     }
+
+    //Post
+    @RequestMapping(method = RequestMethod.POST,
+        consumes = APPLICATION_JSON_VALUE,
+        produces = APPLICATION_JSON_VALUE
+    )
+    public Person create(@RequestBody Person person){
+        return service.create(person);
+    }
+
+    //Put
+    @RequestMapping(method = RequestMethod.PUT,
+            consumes = APPLICATION_JSON_VALUE,
+            produces = APPLICATION_JSON_VALUE
+    )
+    public Person update(@RequestBody Person person){
+        return service.update(person);
+    }
+
+    //Delete
+    @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+    public void delete(@PathVariable("id") String id){
+        service.delete(id);
+
+    }
+
 }
